b6882a0d538dd024735a6d16ddd23f1c313e5fa3
H202 <H2O2email@gmail.com>
2013-09-21 01:24:00 +0100

    stuff

--- StarFrontier/StarFrontier/Player.cs
+++ StarFrontier/StarFrontier/Player.cs
@@ -14,14 +14,18 @@ namespace StarFrontier
     public class Player
     {
         Texture2D PlayerCurrentSprite;
-        public static Vector2 PlayerPos = new Vector2(250,250);
+        public static Vector2 PlayerPos { get; set; }
         public static float PlayerRotation = 0; 
         //for Walking animation
         public static Point FrameSize = new Point(48,48);
         public static Point CurrentFrame = new Point(0, 0);
         public static Point SpriteSheetSize = new Point(4, 2);
         public static Vector2 direction;
-        public static int TimeElapsed = 0; // is used to regulate the speed of the walking animation
+        public static int TimeElapsed2 = 0;// is used to regulate the speed of the backward walking animation
+        public static int TimeElapsed = 0;// is used to regulate the speed of the forward walking animation
+        public static Vector2 SideStrafe; // initial player position saved here when player starts side strafing.
+        public static Vector2 CurrentMousePosition;
+        public static bool IsSideStrafing = false;
         public static void RotatePlayer()
         {
             PlayerRotation = (float)Math.Atan2(Game1.Distance.Y,Game1.Distance.X);
@@ -30,12 +34,13 @@ namespace StarFrontier
         {
             if (Keyboardinput.IsKeyDown(Keys.W))
             {
-                if ((Mouse.GetState().X >= PlayerPos.X + 1 || Mouse.GetState().X <= PlayerPos.X - 1) && (Mouse.GetState().Y >= PlayerPos.Y + 1 || Mouse.GetState().Y <= PlayerPos.Y - 1))
+               
+                if ((Mouse.GetState().X >= PlayerPos.X + 1 || Mouse.GetState().X <= PlayerPos.X - 1) && (Mouse.GetState().Y >= PlayerPos.Y + 1 || Mouse.GetState().Y <= PlayerPos.Y - 1)) // prevents walking bug "spiral of death"
                 {
                     direction = new Vector2((float)Math.Cos(PlayerRotation), (float)Math.Sin(Player.PlayerRotation));
                     PlayerPos -= direction * 1 * (gameTime.ElapsedGameTime.Milliseconds / 6);
                     TimeElapsed += gameTime.ElapsedGameTime.Milliseconds;
-                    if (TimeElapsed > 100)
+                    if (TimeElapsed > 100)// controls walking animation speed
                     {
                         if ((CurrentFrame.X <= 4) && (CurrentFrame.Y == 0))
                             CurrentFrame.X = CurrentFrame.X + 1;
@@ -56,31 +61,71 @@ namespace StarFrontier
                         TimeElapsed = 0;
                     }
                 }
-                else
+                else // resets walking animation frame if player stops due to proximity with mouse pointer.
                 {
                     CurrentFrame.X = 0;
                     CurrentFrame.Y = 0;
                 }
             }
-            if (Keyboardinput.IsKeyUp(Keys.W))
+            if (Keyboardinput.IsKeyUp(Keys.W) && Keyboardinput.IsKeyUp(Keys.S))//resets walking animation frame when player standing still
             {
                 CurrentFrame.X = 0;
                 CurrentFrame.Y = 0;
             }
             if (Keyboardinput.IsKeyDown(Keys.S))
             {
+                
                 direction = new Vector2((float)Math.Cos(PlayerRotation), (float)Math.Sin(Player.PlayerRotation));
-                PlayerPos += direction * 1 * (gameTime.ElapsedGameTime.Milliseconds / 6 );
+                    PlayerPos += direction * 1 * (gameTime.ElapsedGameTime.Milliseconds / 6);
+                    TimeElapsed2 += gameTime.ElapsedGameTime.Milliseconds;
+                    if (TimeElapsed2 > 100)// controls walking animation speed
+                    {
+                        if ((CurrentFrame.X <= 4) && (CurrentFrame.Y == 0))
+                            CurrentFrame.X = CurrentFrame.X + 1;
+                        if (CurrentFrame.X >= 4 && CurrentFrame.Y == 0)
+                        {
+                            CurrentFrame.X = 0;
+                            CurrentFrame.Y = 1;
+                        }
+                        if ((CurrentFrame.X <= 4) && (CurrentFrame.Y == 1))
+                        {
+                            CurrentFrame.X = CurrentFrame.X + 1;
+                        }
+                        if ((CurrentFrame.X >= 4) && (CurrentFrame.Y == 1))
+                        {
+                            CurrentFrame.X = 0;
+                            CurrentFrame.Y = 0;
+                        }
+                        TimeElapsed2 = 0;
+                    }
             }
             if (Keyboardinput.IsKeyDown(Keys.D))//player supposed to move right from mouse direction. NOT WORKING YET
             {
-                direction = new Vector2((float)Math.Cos(PlayerRotation + 1.57f), (float)Math.Sin(Player.PlayerRotation));
-                PlayerPos += direction * 1 * (gameTime.ElapsedGameTime.Milliseconds / 6);
+                if (!IsSideStrafing)
+                {
+ 
+                    SideStrafe = new Vector2((float)Math.Cos(PlayerRotation + 1.5707f), (float)Math.Sin(Player.PlayerRotation + 1.5707f));
+                    IsSideStrafing = true;
+                }
+                PlayerPos -= SideStrafe * 1 * (gameTime.ElapsedGameTime.Milliseconds / 6);
+            }
+            if (Keyboardinput.IsKeyUp(Keys.D) && Keyboardinput.IsKeyUp(Keys.A))//player supposed to move right from mouse direction. NOT WORKING YET
+            {
+                if ((CurrentMousePosition.X != Mouse.GetState().X && CurrentMousePosition.Y != Mouse.GetState().Y))
+                {
+                    CurrentMousePosition.X = Mouse.GetState().X;
+                    CurrentMousePosition.Y = Mouse.GetState().Y;
+                    IsSideStrafing = false;
+                }
             }
             if (Keyboardinput.IsKeyDown(Keys.A))//player supposed to move left from mouse direction. NOT WORKING YET
             {
-                direction = new Vector2((float)Math.Cos(PlayerRotation + 1.57f), (float)Math.Sin(Player.PlayerRotation));
-                PlayerPos += direction * 1 * (gameTime.ElapsedGameTime.Milliseconds / 6);
+                if (!IsSideStrafing || (CurrentMousePosition.X != Mouse.GetState().X && CurrentMousePosition.Y != Mouse.GetState().Y))
+                {
+                    SideStrafe = new Vector2((float)Math.Cos(PlayerRotation + 1.5707f), (float)Math.Sin(Player.PlayerRotation + 1.5707f));
+                    IsSideStrafing = true;
+                }
+                PlayerPos += SideStrafe * 1 * (gameTime.ElapsedGameTime.Milliseconds / 6);
             }
             return PlayerPos;
         }
